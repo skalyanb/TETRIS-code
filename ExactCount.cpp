1c374dd874064dce6b6a7e78726bc07cb7f8fb1c
skalyanb <suman.k.bera.gr@dartmouth.edu>
2019-09-28 15:37:01 -0700

    Added seperate target for computing exact triangles.

---
+++ ExactCount.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Suman Kalyan Bera on 2019-09-28.
+//
+
+#include <iostream>
+
+#include "include/GraphIO.h"
+#include "include/Graph.h"
+#include "include/TriangleEstimators.h"
+
+using namespace Escape;
+int main(int argc, char *argv[]) {
+
+    if (argc!=2) {
+        std::cout << "Usage: ./ExactCount input_file_name";
+        return -1;
+    }
+
+    //Uplaod the graph from input path
+    Graph g;
+    if (loadGraph(argv[1], g, 1, IOFormat::escape))
+        exit(1);
+    printf("#Vertices = %lld, #Edges = %lld\n", g.nVertices, g.nEdges);
+
+    printf("Loaded graph from %s\n", argv[1]);
+    CGraph cg = makeCSR(g);
+    cg.sortById();
+    printf("Converted to CSR\n");
+
+    Estimates out = CountExactTriangles(&cg);
+    std::cout << out.triangle_estimate ;
+    return 0;
+}
